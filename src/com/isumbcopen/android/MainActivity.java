8c27d3102e0b51c12f9b5c055e2cac7f9cd4d3fe
Dylan Whichard <dylan@whichard.com>
2015-02-26 11:11:27 -0500

    Update default textview

--- src/com/isumbcopen/android/MainActivity.java
+++ src/com/isumbcopen/android/MainActivity.java
@@ -2,14 +2,20 @@ package com.isumbcopen.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
-    /** Called when the activity is first created. */
+
+    private TextView status;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+	this.status = (TextView)findViewById(R.id.status);
     }
 }
